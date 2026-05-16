715e67eb9de9d7f4ceb006242e4525c83f008999
MeghaScaria <megha.s.scaria@gmail.com>
2026-05-17 02:49:44 +0530

    Implemented realtime dashboard, websocket sync, and URL analysis

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi import WebSocket, WebSocketDisconnect
 from app.services.websocket_manager import manager
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.config.database import (
     connect_to_mongo,
@@ -32,6 +33,13 @@ app = FastAPI(
     lifespan=lifespan
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # ==========================
 # REQUEST MODELS
@@ -63,7 +71,11 @@ async def analyze_sms(data: SMSRequest):
             **response
         }
     )
-
+    
+    await manager.broadcast({
+        "type": "fraud_update",
+        "data": result
+    })
     return response
 
 
